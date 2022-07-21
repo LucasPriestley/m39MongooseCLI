2f97ac96cecdeffec07c78c096bb454918ff95ac
Lucas <104462643+LucasPriestley@users.noreply.github.com>
2022-07-21 15:44:41 +0100

    CRUD functions added

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 require("./db/connection");
 const yargs = require("yargs");
 const mongoose = require("mongoose");
-const { createMovie } = require("./movie/functions");
+const { createMovie, findMovie, updateMovie, deleteMovie } = require("./movie/functions");
 
 const app = async (yargsObj) => {
   if (yargsObj.create) {
     await createMovie({ title: yargsObj.title, actor: yargsObj.actor });
     //add movie to DB
   } else if (yargsObj.read) {
+    await findMovie({ title: yargsObj.title });
     //list all movies from DB
   } else if (yargsObj.update) {
+    const filterObj = { title: yargsObj.title };
+    const updateObj = { title: yargsObj.newTitle };
+    await updateMovie(filterObj, updateObj);
     //update one movie from DB
   } else if (yargsObj.delete) {
+    await deleteMovie( {title: yargsObj.title} );
     //delete one movie from DB
   } else {
     console.log("Incorrect command");
--- src/movie/functions.js
+++ src/movie/functions.js
@@ -9,3 +9,33 @@ exports.createMovie = async (movieObj) => {
     console.log(error);
   }
 };
+
+exports.findMovie = async (movieObj) => {
+  try {
+    // console.log(movieObj);
+    const findMovie = await Movie.find(movieObj);
+    console.log(findMovie);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.updateMovie = async (filter, update) => {
+  try {
+    console.log("U");
+    const updateMovie = await Movie.updateOne(filter, update);
+
+    console.log(updateMovie);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.deleteMovie = async (movieObj) => {
+  try {
+    console.log("D");
+    await Movie.deleteOne(movieObj);
+  } catch (error) {
+    console.log(error);
+  }
+};
